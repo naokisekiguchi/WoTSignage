3537924c487316e87561b90a265fef9e967f3b21
naokisekiguchi <na-sekiguchi@kddilabs.jp>
2016-10-21 09:50:09 +0900

    led step at class:1st

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+//Lチカ用にLEDポートとLEDの値のためのグローバル変数を定義
+var ledPort;
+var ledValue=0;
 
 // task.js ライブラリ
 const { spawn, sleep } = task;
@@ -6,10 +9,34 @@ document.addEventListener("DOMContentLoaded", () => {
   
   // task.js の spawn 関数内では Promise が同期的に記述できる
   spawn(function() {
+    // GPIO へのアクセサを取得
+    const gpioAccessor = yield navigator.requestGPIOAccess();
+    //GPIO198(CHIRIMEN CN1-9)をLチカ用のGPIOポートとして利用する
+    ledPort = gpioAccessor.ports.get(198);
+    //ledPortを出力として利用する
+    yield ledPort.export("out");
 
+    //リンクをクリックした時のイベントを設定する関数
+    addEventLink();
   });
 
 });
+
+function addEventLink(){
+  //ドキュメント内のaタグを全て取得
+  Array.from(document.querySelectorAll("a"),  (e) => {
+    //取得した要素(aタグ)にtouchstartイベントを設定(CHIRIMENではマウスイベントはtouchイベントに置き換えられる)
+    e.addEventListener("touchstart",()=>{
+      //LEDを点灯させる
+      ledPort.write(1);
+      //一致時間(1000ミリ秒)後の処理を記述
+      setTimeout(()=>{
+        //LEDを消灯させる
+        ledPort.write(0);
+      },1000);
+    });
+  });
+}
 
 
 /*
